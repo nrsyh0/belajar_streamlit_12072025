ba50640b3c56408bec3cdbbe24e61740ba484806
nrsyh0 <hehea640@gmail.com>
2025-07-12 10:07:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,3 +16,5 @@ import numpy as np
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.area_chart(chart_data)
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
