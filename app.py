d030061d0ea4422f65a8f668576754cc92c56fe2
nrsyh0 <hehea640@gmail.com>
2025-07-12 10:23:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,3 +35,7 @@ with col3:
     st.header("An owl")
     st.image("https://static.streamlit.io/examples/owl.jpg")
 
+with st.sidebar:
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
+
