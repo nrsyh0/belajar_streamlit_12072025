e56c973ced957e1f39d6d6e1eb621569f60673c7
nrsyh0 <hehea640@gmail.com>
2025-07-12 10:18:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,3 +21,14 @@ if st.button("Say hello"):
     st.write("Why hello there")
 else:
     st.write("Goodbye")
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
